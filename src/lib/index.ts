c516a48a59d544e32c9048352e1f66862278865a
fallenoak <git@fallenoak.me>
2023-12-27 23:01:34 -0600

    feat(texture): add TextureManager

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@ export * from './util.js';
 export * from './controls/OrbitControls.js';
 export * from './AssetManager.js';
 export * from './FormatManager.js';
+export * from './TextureManager.js';
